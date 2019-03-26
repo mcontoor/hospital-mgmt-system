18c844831fc4a008180c756dff7d6809e3395c11
aditya3438 <aditya3438@gmail.com>
2019-03-26 15:00:29 +0530

    added css and js directories

---
+++ submissions/weeklydietplanner/server.js
@@ -0,0 +1,26 @@
+//install dependencies and create a simple node server
+var express =require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app =express();
+
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 3001;
+
+
+app.listen(port, function(){
+    console.log('Server is listening on '+ port +' ...');
+
+});
+
+app.get('/',function(req,res){
+
+    res.send("HELLO WORLD");
+});
+
+// connect the server to MongoDB database 'slackernews' using mongoose
+
+// mongoose.connect('mongodb://localhost:27017/slackernews', {useNewUrlParser: true});
