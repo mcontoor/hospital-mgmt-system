dac756a75dc97983b8a42cb910d7eec3f6d78eb8
Your Name <you@example.com>
2019-03-26 18:54:16 +0530

    first Schema

---
+++ submissions/weeklydietplanner/model/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var userSchema =new Schema({
+firstname:{
+    type:String,
+    required:true
+}, 
+lastname:{
+    type:String,
+     required:true
+
+},
+username:{
+    type:String,
+     required:true
+
+},
+password:{
+    type:String,
+     required:true
+}
+});
+module.exports = mongoose.model('User',userSchema);
