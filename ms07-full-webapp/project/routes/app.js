267d3e9d7224d9b474706e0b5ecc8e8566aef05d
SaiChandra <saichandra2896@gmail.com>
2019-03-27 16:59:38 +0530

    adding routes

---
+++ ms07-full-webapp/project/routes/app.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const Doctor = require('../models/doctor');
+const User = require('../models/user');
+const session = require('express-session');
+//route to home page
+router.get('/home',(req,res)=>res.render('home.ejs'));
+//route to login page
+router.get('/login',(req,res)=>res.render('login.ejs'));
+//route to register page
+router.get('/register',(req,res)=>res.render('register.ejs'));
+//adding user to database
+router.post('/register',function(req,res){
+    console.log(req.body);
+    let user = new User();
+    user.firstname = req.body.firstname;
+    user.lastname = req.body.lastname;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.age = req.body.age;
+    user.email = req.body.emailId;
+    user.save().then(console.log("New User Created",user)).catch(err=>console.log(err));
+    res.redirect('/login');
+});
+//user login
+router.post('/login',function(req,res){
+   let username = req.body.username;
+   let password = req.body.password;
+   User.findOne({username : username, password : password},function(err,user){
+       if(err){
+           console.log(err);
+       }
+       if(!user){
+           return res.status(404).send('<h1>User Not Found</h1>');
+       }
+       return res.render(`<h1>Welcome ${username}</h1>`);
+   });
+   res.render('home');
+});
+module.exports = router;
